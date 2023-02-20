c93965757f6f6d1a77fd9a4a1056e4cbc8bf7048
JuanCruzEusebi <juancruzeusebi@gmail.com>
2023-02-20 12:46:25 -0300

    fixed api call

--- src/components/WeatherApp.js
+++ src/components/WeatherApp.js
@@ -5,6 +5,7 @@ export default function WeatherApp() {
 
     const [weather, setWeather] = useState(null)
 
+
     async function loadInfo(city = 'london') {
         try {
             const req = await fetch(`http://api.weatherapi.com/v1/current.json?key=173976480d4e46fba26144654232002&q=${city}&aqi=no`)
@@ -21,9 +22,19 @@ export default function WeatherApp() {
         loadInfo(city);
     }
 
+
     return (
         <div>
             <WeatherForm onChangeCity={handleChangeCity} />
+            {weather &&
+                <div>
+                    <h2>{weather.location.name}, {weather.location.region}, {weather.location.country}</h2>
+                    <h3>{weather.current.condition.text}</h3>
+                    <p>Temperature: {weather.current.temp_c} C</p>
+                    <p>Feels like: {weather.current.feelslike_c} C</p>
+                    <p>Wind speed: {weather.current.wind_kph} km/h</p>
+                </div>
+            }
         </div>
     )
 }
